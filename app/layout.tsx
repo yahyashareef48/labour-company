3ce074c48c47aa2848a4f820f8383471169bf6f3
YahyaShareef <yahyashareef48@gmail.com>
2024-06-20 22:55:18 +0530

    miner optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/hero-bg.webp" as="image" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
+
+// #ffe101 (Original color)
+// #fff2cc (Slightly lighter shade)
+// #fded9b (Lighter shade)
+// #fce98c (Even lighter shade)
+// #fde47c (Very light shade)
+// #fcd96c (Light shade with a touch more yellow)
+// #fdc45c (Lighter shade with a touch more yellow)
+// #fcab4c (Even lighter shade with a touch more yellow)
+// #fca03c (Very light shade with a touch more yellow)
+// #f9972c (Lightest shade with a significant amount of yellow)
